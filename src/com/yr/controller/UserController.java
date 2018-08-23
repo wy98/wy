f75d2a070c3caa42014549b96c43685c1760889c
wy98 <1958134184@qq.com>
2018-08-23 22:36:16 +0800

    dd

--- src/com/yr/controller/UserController.java
+++ src/com/yr/controller/UserController.java
@@ -2,7 +2,9 @@ package com.yr.controller;
 
 public class UserController  {
  public static void uu(){
-	 System.out.println(12366);
+	 System.out.println("dd");
+	 System.out.println(123669);
+	 System.out.println("12366oooooooooo");
 	 System.out.println("========----==");
 	 System.out.println("========----==");
  }
