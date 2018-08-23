7d31264d465360b08ec3c890ef280b277f540309
wy98 <1958134184@qq.com>
2018-08-23 21:54:43 +0800

    eclipse提交的第一个git项目

---
+++ src/com/yr/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.yr.controller;
+
+public class UserController  {
+ public static void uu(){
+	 System.out.println(4444);
+ }
+}
---
+++ src/com/yr/test/TestMain.java
@@ -0,0 +1,13 @@
+package com.yr.test;
+
+import com.yr.controller.UserController;
+
+public class TestMain {
+ public static void main(String[] args) {
+	TestMain.aa();
+}
+ public static void aa(){
+	 System.out.println(333);
+	 UserController.uu();
+ }
+}
