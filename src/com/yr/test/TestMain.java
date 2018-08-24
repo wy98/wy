67300ae299e13d7b32e466481a004c6eaf29f2d7
wy98 <1958134184@qq.com>
2018-08-24 09:18:33 +0800

    冲突

--- src/com/yr/test/TestMain.java
+++ src/com/yr/test/TestMain.java
@@ -7,10 +7,9 @@ public class TestMain {
 	TestMain.aa();
 }
  public static void aa(){
-	 System.out.println(4445555);
-	 System.out.println(33344422);
-	 System.out.println(333444555);
-	 System.out.println(33344422);
+	 System.out.println(41115555);
+	 System.out.println(333);
+	 System.out.println(4445588);	 
 	 UserController.uu();
  }
 }
