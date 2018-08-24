87699dccaf20949be9291ed3cfed734d04cf13eb
wy98 <42628655+wy98@users.noreply.github.com>
2018-08-24 09:19:59 +0800

    Update TestMain.java

--- src/com/yr/test/TestMain.java
+++ src/com/yr/test/TestMain.java
@@ -7,9 +7,6 @@ public class TestMain {
 	TestMain.aa();
 }
  public static void aa(){
-	 System.out.println(41115555);
-	 System.out.println(333);
-	 System.out.println(4445588);	 
 	 System.out.println(4445588);	 
 	 UserController.uu();
  }
