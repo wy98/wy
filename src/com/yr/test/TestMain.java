b1986dcefb536a81444453b584bf64675344cc47
wy98 <1958134184@qq.com>
2018-08-24 09:22:58 +0800

    ww

--- src/com/yr/test/TestMain.java
+++ src/com/yr/test/TestMain.java
@@ -8,9 +8,7 @@ public class TestMain {
 }
  public static void aa(){
 	 System.out.println(41115555);
-	 System.out.println(333);
-	 System.out.println(4445588);	 
-	 System.out.println(4445588);	 
+	 System.out.println(321);	 
 	 UserController.uu();
  }
 }
